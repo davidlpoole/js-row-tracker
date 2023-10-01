c4802e1a3a91420dc3cf78069015729374bd0c5f
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-10-01 18:11:01 +1300

    display saved data

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
+'use strict'
+
 function app() {
   const data = []
   const btnSave = document.getElementById('save')
   btnSave.onclick = () => {
     const record = save()
     data.push(record)
+    displayData(data)
   }
+}
+
+function displayData(data) {
+  const results = document.getElementById('results')
+  results.innerHTML = JSON.stringify(data)
 }
 
 function save() {
@@ -12,6 +20,7 @@ function save() {
   const inputs = document.getElementsByTagName('input')
   for (let i = 0; i < inputs.length; i++) {
     temp[inputs[i].id] = inputs[i].value
+    inputs[i].value = ''
   }
   return temp
 }
