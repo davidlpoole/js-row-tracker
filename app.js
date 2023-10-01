7d21ee5afdd1eb525b6876d6ca7e2acd06f05421
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-10-01 21:16:43 +1300

    Get list of unique puller/roller

--- app.js
+++ app.js
@@ -4,26 +4,48 @@ function app() {
   const data = []
 
   const inputs = document.getElementsByTagName('input')
-  // const btnSave = document.getElementById('save')
-  const btnNext = document.getElementById('next')
 
-  // btnSave.onclick = () => {
-  //   const record = save(inputs)
-  //   reset(inputs)
+  // const btnNext = document.getElementById('next')
+  // btnNext.onclick = () => {
+  //   const record = nextRow(inputs)
   //   data.push(record)
   //   displayData(data)
   // }
 
-  btnNext.onclick = () => {
-    const record = nextRow(inputs)
+  const btnSave = document.getElementById('save')
+  btnSave.onclick = () => {
+    const record = save(inputs)
+    reset(inputs)
     data.push(record)
     displayData(data)
+    appendList('pullers', data, 'puller')
+    appendList('rollers', data, 'roller')
   }
 }
 
 function displayData(data) {
   const results = document.getElementById('results')
-  results.innerHTML = JSON.stringify(data)
+  results.replaceChildren()
+  const ul = results.appendChild(document.createElement('ul'))
+  data.forEach((element) => {
+    const li = ul.appendChild(document.createElement('li'))
+    li.innerHTML = JSON.stringify(element)
+  })
+}
+
+function appendList(parentId, data, itemId) {
+  const parent = document.getElementById(parentId)
+  parent.replaceChildren()
+  const ul = parent.appendChild(document.createElement('ul'))
+
+  const list = data.map((item) => item[itemId])
+  const unique = list.filter((v, i, a) => a.indexOf(v) == i)
+  const sorted = unique.toSorted((a, b) => a > b)
+
+  sorted.forEach((element) => {
+    const li = ul.appendChild(document.createElement('li'))
+    li.innerHTML = element
+  })
 }
 
 function save(inputs) {
@@ -40,17 +62,17 @@ function reset(inputs) {
   }
 }
 
-function nextRow(inputs) {
-  const record = save(inputs)
-  const resetIds = ['puller', 'roller1', 'roller2']
-  for (let i = 0; i < inputs.length; i++) {
-    if (resetIds.includes(inputs[i].id)) {
-      inputs[i].value = ''
-    } else if (inputs[i].id === 'row') {
-      inputs[i].value++
-    }
-  }
-  return record
-}
+// function nextRow(inputs) {
+//   const record = save(inputs)
+//   const resetIds = ['puller', 'roller1', 'roller2']
+//   for (let i = 0; i < inputs.length; i++) {
+//     if (resetIds.includes(inputs[i].id)) {
+//       inputs[i].value = ''
+//     } else if (inputs[i].id === 'row') {
+//       inputs[i].value++
+//     }
+//   }
+//   return record
+// }
 
 app()
